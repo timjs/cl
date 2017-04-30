746e62cbfa095330e7aac096fc94c250a50a694d
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2017-04-30 10:34:24 +0200

    Add logging capabilities

--- src/cl.cr
+++ src/cl.cr
@@ -1,5 +1,26 @@
-require "./cl/*"
+require "logger"
 
-module Cl
-  # TODO Put your code here
+# Logger #######################################################################
+
+LOG = Logger.new(STDOUT)
+LOG.level = Logger::INFO
+LOG.formatter = Logger::Formatter.new do |severity, _datetime, _progname, message, io|
+  io << case severity
+  when "DEBUG"
+    ":: "
+  when "INFO"
+    ">> "
+  when "WARN"
+    "** "
+  when "ERROR"
+    "!! "
+  when "FATAL"
+    "   " # FIXME: Bullet for FATAL
+  end
+  io << message
+  io << "..." if severity == "INFO"
 end
+
+# Main #########################################################################
+
+LOG.info("Help!")
