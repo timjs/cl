466151cfdf462940a76f7ef4f67fc1b5ffe241c8
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2017-04-30 10:36:04 +0200

    Add colours to log messages

--- src/cl.cr
+++ src/cl.cr
@@ -1,4 +1,5 @@
 require "logger"
+require "colorize"
 
 # Logger #######################################################################
 
@@ -7,13 +8,13 @@ LOG.level = Logger::INFO
 LOG.formatter = Logger::Formatter.new do |severity, _datetime, _progname, message, io|
   io << case severity
   when "DEBUG"
-    ":: "
+    ":: ".colorize.white
   when "INFO"
-    ">> "
+    ">> ".colorize.green
   when "WARN"
-    "** "
+    "** ".colorize.yellow
   when "ERROR"
-    "!! "
+    "!! ".colorize.red
   when "FATAL"
     "   " # FIXME: Bullet for FATAL
   end
